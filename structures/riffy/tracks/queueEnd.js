5fe53e0ea1b70568d8aa21147cf21b94ef37984e
Soumy Agarwal <mukundagrawal800@gmail.com>
2024-10-21 20:58:05 +0530

    Update queueEnd.js

--- structures/riffy/tracks/queueEnd.js
+++ structures/riffy/tracks/queueEnd.js
@@ -4,11 +4,6 @@ client.riffy.on("queueEnd", async (player) => {
     const channel = client.channels.cache.get(player.textChannel);
     
     if (player.message) await player.message.delete();
-
-    if (player.isAutoplay) {
-        player.autoplay(player)
-    } else {
-        player.destroy();
-        channel.send("Queue has ended.");
-    }
+    
+        player.autoplay(player);
 })
