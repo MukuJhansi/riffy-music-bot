a28f3514365b36fedc8ea27f05d48e02f64d0f22
MukuJhansi <mukundagrawal800@gmail.com>
2024-10-22 19:43:43 +0530

    updates

--- structures/events/client/ready.js
+++ structures/events/client/ready.js
@@ -3,28 +3,47 @@ const client = require("../../client");
 const { logger } = require("../../functions/logger");
 
 client.on("ready", async () => {
-    client.riffy.init(client.user.id);
+    client.riffy.init(client.user.id); // Ensure this doesn't block presence updates
 
-    console.log("\n---------------------")
-    logger(`${client.user.tag} is ready`, "success")
-    console.log("---------------------")
+    console.log("\n---------------------");
+    logger(`${client.user.tag} is ready`, "success");
+    console.log("---------------------");
 
-    client.user.setPresence({
-        activities: [
-			{
-				name: "{Guilds} servers",
-				type: "WATCHING",
-				data: (client) => {
-					return {
-						Guilds: client.guilds.cache.size,
-					};
-				},
-			},
-			{
-				name: "Playing",
-				type: "Made By Gunman",
-			},
-		],
-        status: "dnd"
-    })
-})
+    // Function to update presence
+    const updatePresence = async () => {
+        let totalMembers = 0;
+        const serverCount = client.guilds.cache.size; // Get the number of servers
+
+        // Fetch total members from all guilds
+        for (const guild of client.guilds.cache.values()) {
+            // Fetch all members in the guild
+            await guild.members.fetch(); // This will cache all members in the guild
+            totalMembers += guild.memberCount; // Add the member count of the current guild
+        }
+
+        // Set the presence with the desired format
+        const activities = [
+            {
+                name: `${totalMembers} Users Over ${serverCount} Servers`, // Activity 1
+                type: ActivityType.Watching,
+            },
+            {
+                name: "Made By Gunman", // Activity 2
+                type: ActivityType.Playing,
+            },
+        ];
+
+        // Determine which presence to show (0 or 1)
+        const currentActivityIndex = Math.floor((Date.now() / 10000) % 2); // Switch every 10 seconds
+        client.user.setPresence({
+            status: "dnd",
+            activities: [activities[currentActivityIndex]],
+        });
+    };
+
+    // Initial presence update
+    await updatePresence();
+
+    // Update presence every 10 seconds
+    setInterval(updatePresence, 10000);
+});
--- structures/riffy/tracks/trackStart.js
+++ structures/riffy/tracks/trackStart.js
@@ -1,6 +1,6 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const { Dynamic } = require("musicard");
-const client = require("../../client")
+const client = require("../../client");
 
 client.riffy.on('trackStart', async (player, track) => {
     const row = new ActionRowBuilder()
@@ -9,12 +9,10 @@ client.riffy.on('trackStart', async (player, track) => {
                 .setCustomId('disconnect')
                 .setStyle(ButtonStyle.Secondary)
                 .setEmoji('⏺'),
-
             new ButtonBuilder()
                 .setCustomId('pause')
                 .setStyle(ButtonStyle.Secondary)
                 .setEmoji('⏸'),
-
             new ButtonBuilder()
                 .setCustomId('skip')
                 .setStyle(ButtonStyle.Secondary)
@@ -23,50 +21,49 @@ client.riffy.on('trackStart', async (player, track) => {
 
     const channel = client.channels.cache.get(player.textChannel);
 
-    function formatTime(time) {
-        const minutes = Math.floor(time / 60);
-        const seconds = Math.floor(time % 60);
-        return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-    }
+    // Total length of the track in seconds
+    const totalLength = Math.round(track.info.length / 1000); // Convert from ms to seconds
 
-    const musicLength = track.info.length;
-    const formattedLength = formatTime(Math.round(musicLength / 1000));
-    const [minutesStr, secondsStr] = formattedLength.split(":");
-    const minutes = parseInt(minutesStr, 10);
-    const seconds = parseInt(secondsStr, 10);
-    const totalMilliseconds = (minutes * 60 + seconds) * 1000;
-
-    //disabling buttons when the song ends
-    const rowDisabled = new ActionRowBuilder()
-        .addComponents(
-            new ButtonBuilder()
-                .setCustomId('disconnect')
-                .setStyle(ButtonStyle.Secondary)
-                .setEmoji('⏺')
-                .setDisabled(true),
-
-            new ButtonBuilder()
-                .setCustomId('pause')
-                .setStyle(ButtonStyle.Secondary)
-                .setEmoji('⏸')
-                .setDisabled(true),
-
-            new ButtonBuilder()
-                .setCustomId('skip')
-                .setStyle(ButtonStyle.Secondary)
-                .setEmoji('⏭')
-                .setDisabled(true)
-        );
-
-    const musicard = await Dynamic({
+    // Create the initial music card with elapsed time
+    const musicCardBuffer = await Dynamic({
         thumbnailImage: track.info.thumbnail,
-        name: track.info.title,
-        author: track.info.author
+        name: `${track.info.title}`,
+        author: track.info.author,
+        progress: 0 // Initial progress
     });
 
-    const msg = await channel
-    .send({
-        files: [{attachment: musicard}],
-        components: [row]})
-    .then((x) => (player.message = x));
+    // Send the initial music card message
+    const msg = await channel.send({
+        files: [{ attachment: musicCardBuffer, name: `musicard.png` }],
+        components: [row]
+    });
+
+    // Store the message and track in the player object
+    player.message = msg;
+    player.track = track;
+
+    let elapsedTime = 0; // Track elapsed time in seconds
+    const intervalId = setInterval(async () => {
+        if (player.playing) {
+            elapsedTime++;
+
+            // Calculate the progress percentage
+            const progress = Math.min((elapsedTime / totalLength) * 100, 100); // Clamp to 100%
+
+            // Update the music card with new elapsed time and progress
+            const updatedMusicCardBuffer = await Dynamic({
+                thumbnailImage: track.info.thumbnail,
+                name: `${track.info.title}`,
+                author: track.info.author,
+                progress: progress // Pass the progress for visual representation
+            });
+
+            // Edit the existing message with updated elapsed time
+            await player.message.edit({
+                files: [{ attachment: updatedMusicCardBuffer, name: `musicard.png` }],
+                components: [row] // Keep buttons active
+            });
+        }
+    }, 1000); // Update every 1 second
+
 });
